ff25524e66f9a14281893826a4e0e7c7051a9e0d
gus97 <13851628057@139.com>
2017-12-05 17:25:10 +0800

    up

--- spark/src/main/scala/cn/gus/core/SparkStruct4Kafka.scala
+++ spark/src/main/scala/cn/gus/core/SparkStruct4Kafka.scala
@@ -1,11 +1,14 @@
 package cn.gus.core
 
-
-
 import org.apache.spark.sql.streaming.ProcessingTime
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
 import org.apache.spark.sql.{ForeachWriter, Row, SparkSession}
 
+import scala.reflect.internal.util.TableDef.Column
+
+
 object SparkStruct4Kafka {
+
 
   def main(args: Array[String]): Unit = {
 
@@ -18,14 +21,14 @@ object SparkStruct4Kafka {
     val spark = SparkSession
       .builder()
       .appName("gus-1111")
-      .master("spark://172.18.111.3:7078")
-      //.master("local[8]")
+      //.master("spark://172.18.111.3:7078")
+      .master("local[8]")
       //.enableHiveSupport()
       .config("spark.executor.memory", "512m")
       .config("spark.driver.cores", 1)
       .config("spark.cores.max", 4)
       .config("spark.driver.memory", "512m")
-      .config("spark.driver.host", "172.16.39.52")
+      //.config("spark.driver.host", "172.16.39.52")
       .config("spark.ui.port", 4051)
       .config("spark.eventLog.dir", "hdfs://172.18.111.3:9000/spark-2.2.0/applicationHistory")
       .config("spark.eventLog.enabled", value = true)
@@ -33,33 +36,41 @@ object SparkStruct4Kafka {
       .config("spark.logConf", value = true)
       .getOrCreate()
 
-    import spark.implicits._
 
-//    spark.sql("use gus").show
-//    spark.sql("show tables").show
-//
-//    spark.sql("select count(1) from emp").show
+    //import spark.implicits._
 
+    implicit val mapEncoder = org.apache.spark.sql.Encoders.kryo[Map[Any, String]]
 
-    val checkpointLocation = "hdfs://172.18.111.3:9000/tmp/checkpointLocation-spark01"
+    val checkpointLocation = "hdfs://172.18.111.3:9000/tmp/checkpointLocation-spark001"
 
     val lines = spark
       .readStream
       .format("kafka")
       .option("kafka.bootstrap.servers", "172.18.111.7:9092,172.18.111.8:9092,172.18.111.9:9092")
       .option("subscribe", "spark01")
-      //.option("startingOffsets", "{\"spark01\":{\"0\":360}}")
-      //.option("startingOffsets", "earliest") //latest
-      .load()
-      .selectExpr("CAST(value AS STRING)")//,"CAST(topic AS STRING)","CAST(partition AS STRING)","CAST(offset AS STRING)")
+//      .schema(schema)
+        .load()
+      .selectExpr("CAST(value AS STRING)")
       .as[(String)]
 
+    //==================================================================================================
 
-    lines.printSchema()
+    val schemaString = "eno ename"
+    val fields = schemaString.split(" ")
+      .map(fieldName => StructField(fieldName, StringType))
+    val schema = StructType(fields)
+    val rowRDD = lines
+      .map(_.split(","))
+      .map(x => Row(x(0), x(1), x(2), x(3), x(4)))
+    //spark.createDataFrame(rowRDD, schema).createOrReplaceTempView("foo")
+    //==================================================================================================
 
-    val wordCounts = lines.flatMap(_.split(" ")).groupBy("value").count()
+    lines.map(_.split(",")).createOrReplaceTempView("foo")
+
+    val wordCounts = spark.sql("SELECT * FROM foo ")
 
 
+    //val wordCounts = lines.flatMap(_.split(" ")).groupBy("value").count
 
     /**
       * .format("parquet") \
@@ -69,39 +80,26 @@ object SparkStruct4Kafka {
     //var i = 0
     val query = wordCounts.writeStream.trigger(ProcessingTime(5000L))
       //complete,append,update
-      .outputMode("update")
-        .option("checkpointLocation", checkpointLocation)
-
-        .foreach(new ForeachWriter[Row] {
-
-        //var fileWriter:FileWriter = _//new FileWriter(new File(s"c:/data/spark/temp"))
+      .outputMode("append")
+      .option("checkpointLocation", checkpointLocation)
+      .foreach(new ForeachWriter[Row] {
 
         override def process(value: Row): Unit = {
 
-          println(value,"========>>",value.toSeq.mkString(","))
+          println(value, "========>>", value.toSeq.mkString(","))
 
-
-          //fileWriter.append(value.toSeq.mkString(","))
-          //i += 1
-          //println(i,"----------->>")
         }
 
         override def close(errorOrNull: Throwable): Unit = {
-          //fileWriter.close()
-          //i -=1
+
 
         }
 
         override def open(partitionId: Long, version: Long): Boolean = {
 
-          //println(partitionId,"===============>>>>>>>>>>",version)
           true
         }
-
-        //println(123)
-
       }).start()
-
     query.awaitTermination()
   }
 }
